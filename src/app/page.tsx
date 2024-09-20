0f7bdd8d2d5d30306734adf1a283e3c0c60accc7
Thiago Carvalho <thiagoafonso07@hotmail.com>
2024-09-20 19:26:35 -0300

    Feat: Adiciona exemplo de componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import NewText from "./components/NewText/NewText";
+ 
 export default function Home() {
   return (
-    <main>
-      que isso
-    </main>
+    <div>
+      <main>
+        <NewText color="red" text="Logan" />
+        <NewText color="blue" text="Thiago" />
+      </main>
+    </div>
   );
 }
