48961e1999a0e019cd35da90d57769a2e50d4dec
logangaabriel <gabriellogancourse@gmail.com>
2024-09-21 23:45:25 -0300

    feat: título e input do ToDo List

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import NewText from "./components/NewText/NewText";
- 
+
 export default function Home() {
   return (
-    <div>
-      <main>
-        <NewText color="red" text="Logan" />
-        <NewText color="blue" text="Thiago" />
-      </main>
+    <div style={{ textAlign: 'center', marginTop: '200px' }}>
+      <h1 style={{color: '#fff'}}>Today main focus</h1>
+      <div style={{ marginTop: '20px' }}>
+        <input 
+          type="text" 
+          placeholder="What is your next task?" 
+          style={{
+            padding: '15px',
+            width: '600px',
+            fontSize: '20px',
+            borderRadius: '10px',
+            border: '1px solid #ccc'
+          }}
+        />
+      </div>
     </div>
   );
 }
